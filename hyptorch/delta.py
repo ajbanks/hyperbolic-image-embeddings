f8c961b5648979f834439ca437a4f5c4489d4d80
ajbanks <ajbanks11@gmail.com>
2021-08-07 10:49:47 +0000

    commit messsyibsbsdsage

--- hyptorch/delta.py
+++ hyptorch/delta.py
@@ -15,57 +15,24 @@ def delta_hyp(dismat):
     """
 
     p = 0
-    # [0]  # means line 0 of your matrix
-    # [(0, 0)]  # means cell at 0,0 of your matrix
-    # [0: 1]  # means lines 0 to 1 excluded of your matrix
-    # [: 1]  # excluding the first value means all lines until line 1 excluded
-    # [1:]  # excluding the last param mean all lines starting form line 1 included
-    # [:]  # excluding both means all lines
-    # [::2]  # the addition of a second ':' is the sampling. (1 item every 2)
-    # [::]  # exluding it means a sampling of 1
-    # [:,:]  # simply uses a tuple (a single , represents an empty tuple) instead of an index.
-    # np.newaxis adds a new axis to an arrway, essential giving it another dimention. a 1D rray will become 2D for example
-    # https://stackoverflow.com/questions/29241056/how-does-numpy-newaxis-work-and-when-to-use-it
-
     row = dismat[p, :][np.newaxis, :]
-    print("\n\n extracted row from dismat: ")
-    print(row)
-
     col = dismat[:, p][:, np.newaxis]
-    print("\n\n extracted column from dismat: ")
-    print(col)
-
     XY_p = 0.5 * (row + col - dismat)
-    print("\n\n performe calc: 0.5 * (row + col - dismat): \n")
-    print(XY_p)
 
-    minimum = np.minimum(XY_p[:, :, None], XY_p[None, :, :])
-    print("\n\n performe calc: np.minimum(XY_p[:, :, None], XY_p[None, :, :]): \n")
-    print(minimum)
-
-
-    maxmin = np.max(minimum, axis=1)
-    print("\n\n performe calc: np.max(minimum, axis=1): \n")
-    print(maxmin)
-
-    # maxmin = np.max(np.minimum(XY_p[:, :, None], XY_p[None, :, :]), axis=1)
-    print("\n\n calculated maxmin")
-
+    maxmin = np.max(np.minimum(XY_p[:, :, None], XY_p[None, :, :]), axis=1)
     return np.max(maxmin - XY_p)
 
 
-def batched_delta_hyp(X, n_tries=5, batch_size=1500):
+def batched_delta_hyp(X, n_tries=10, batch_size=1500):
     vals = []
     for i in tqdm(range(n_tries)):
         idx = np.random.choice(len(X), batch_size)
         X_batch = X[idx]
         distmat = distance_matrix(X_batch, X_batch)
-        print("calcularted batch distance matrix \n")
         diam = np.max(distmat)
-        delta_rel = delta_hyp(distmat) / diam
-        print("\n\n calcularted batch delta: " + str(delta_rel))
+        delta_rel = (2 * delta_hyp(distmat)) / diam
         vals.append(delta_rel)
-    return np.mean(vals) #, np.std(vals)
+    return np.mean(vals), np.std(vals)
 
 
 class Flatten(nn.Module):
@@ -105,7 +72,7 @@ def get_delta(loader):
     print("\n\n all features processed \n\n")
 
     all_features = np.concatenate(all_features)
-    idx = np.random.choice(len(all_features), 50)
+    idx = np.random.choice(len(all_features), 1000)
     all_features_small = all_features[idx]
     print(all_features.shape)
     # print(len(all_features[0]))
@@ -121,6 +88,42 @@ def get_delta(loader):
     print("\n\n calculated hyperbolicity \n\n")
     # diam = np.max(dists)
     return delta #, diam
+
+
+def get_delta_original(loader):
+    """
+    computes delta value for image data by extracting features using VGG network;
+    input -- data loader for images
+    """
+    vgg = torchvision.models.vgg19(pretrained=True)
+    vgg_feats = vgg.features
+    vgg_classifier = nn.Sequential(*list(vgg.classifier.children())[:-1])
+
+    vgg_part = nn.Sequential(vgg_feats, Flatten(), vgg_classifier).to(device)
+    vgg_part.eval()
+
+    all_features = []
+    for i, (batch, _) in enumerate(loader):
+        with torch.no_grad():
+            batch = batch.to(device)
+            all_features.append(vgg_part(batch).detach().cpu().numpy())
+
+    all_features = np.concatenate(all_features)
+    idx = np.random.choice(len(all_features), 50)
+    all_features_small = all_features[idx]
+    # distance_matrix Returns the matrix of all pair-wise distances between two arrays
+    dists = distance_matrix(all_features_small, all_features_small)
+    delta = delta_hyp(dists)
+    """np.max is just an alias for np.amax. This function only works on a 
+    single input array and finds the value of maximum element in that entire
+    array (returning a scalar). Alternatively, it takes an axis argument 
+    and will find the maximum value along an axis 
+    of the input array (returning a new array).
+    https://stackoverflow.com/questions/33569668/numpy-max-vs-amax-vs-maximum
+    """
+    diam = np.max(dists)
+    return (2 * delta) / diam
+    # return delta, diam
 
 test_matrix = [[0,0],[0,1], [1,0],[1,1]]
 test_matrix = np.random.rand(224,224)
--- test_hyperbolicity.py
+++ test_hyperbolicity.py
@@ -14,7 +14,7 @@ def test_salicon_hyperbolicity():
     device = torch.device("cuda" if use_cuda else "cpu")
     kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
     train_loader = DataLoader(
-        SALICON(mode='train', N=100),
+        SALICON(mode='train', N=5000),
         batch_size=10,
         shuffle=True,
         **kwargs
@@ -31,9 +31,9 @@ def test_cifar_hyperbolicity():
     batch_size = 32
     
     
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    odds = list(range(1, len(trainset) - 9000, 2))
+    odds = list(range(1, int(len(trainset) * 0.8) , 2))
     trainset_1 = torch.utils.data.Subset(trainset, odds)
     trainloader = torch.utils.data.DataLoader(trainset_1, batch_size=batch_size,
                                               shuffle=True, num_workers=2)
@@ -48,12 +48,53 @@ def test_cifar_hyperbolicity():
 
     return delta.get_delta(trainloader)
 
+def test_salicon_hyperbolicity_original():
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
+    train_loader = DataLoader(
+        SALICON(mode='train', N=100),
+        batch_size=10,
+        shuffle=True,
+        **kwargs
+    )
+
+    return delta.get_delta_original(train_loader)
+
+def test_cifar_hyperbolicity_original():
+    use_cuda = torch.cuda.is_available()
+    kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
+    transform = transforms.Compose(
+        [transforms.Resize((224,224),interpolation=Image.NEAREST), transforms.ToTensor()])
+
+    batch_size = 32
+    
+    
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                            download=True, transform=transform)
+    odds = list(range(1, len(trainset) - 9800, 2))
+    trainset_1 = torch.utils.data.Subset(trainset, odds)
+    trainloader = torch.utils.data.DataLoader(trainset_1, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+
+    # for images, _ in trainloader:
+    #     print('images.shape:', images.shape)
+    #     plt.figure(figsize=(16, 8))
+    #     plt.axis('off')
+    #     plt.imshow(make_grid(images, nrow=16).permute((1, 2, 0)))
+    #     break
+
+    return delta.get_delta_original(trainloader)
+
+    
+
 
 def run():
     torch.multiprocessing.freeze_support()
-    print( "the hyperbolicity of cifar is: " + str(test_cifar_hyperbolicity()))
-    print("the hyperbolicity of SALICON is: " + str(test_salicon_hyperbolicity()))
-    print( "the hyperbolicity of cifar is: " + str(test_cifar_hyperbolicity()))
+    print( "the hyperbolicity of cifar is: " + str(test_cifar_hyperbolicity_original()))
+    # print("the hyperbolicity of SALICON is: " + str(test_salicon_hyperbolicity_original()))
+    # print( "the hyperbolicity of cifar is: " + str(test_cifar_hyperbolicity()))
 
 if __name__ == '__main__':
     run()
